9051b6637dc0a7fceb73aa923de3c25741a61a72
RaymundT@ny.rga.com <raymund.tuquero@rga.com>
2013-07-03 18:39:40 +0800

    setup mongodb and node

--- web.js
+++ web.js
@@ -1,10 +1,19 @@
-var express = require("express");
+var express = require('express'),
+    wines = require('./routes/wines');
+ 
 var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+ 
+app.configure(function () {
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
 });
+ 
+app.get('/wines', wines.findAll);
+app.get('/wines/:id', wines.findById);
+app.post('/wines', wines.addWine);
+app.put('/wines/:id', wines.updateWine);
+app.delete('/wines/:id', wines.deleteWine);
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
